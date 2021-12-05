3e23ca7c94c4fda513c99d8caf2d88347acbfad8
GbNok <gnokwsie@gmail.com>
2021-12-05 15:46:58 -0300

    Add upload solutions

--- estudio_mate/forms.py
+++ estudio_mate/forms.py
@@ -3,3 +3,5 @@ from django import forms
 class ProblemUpload(forms.Form):
     title = forms.CharField(max_length=200)
     file = forms.FileField()
+    subject = forms.CharField(max_length=200)
+    problem = forms.CharField(max_length=200)
--- estudio_mate/views.py
+++ estudio_mate/views.py
@@ -1,9 +1,15 @@
-from django.http import HttpResponse
 from .models import Subject, Problem
 from django.shortcuts import render
 from .funciones import generate_problems
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from .forms import ProblemUpload
+
+def handle_uploaded_file(f):
+    with open('/static/estudio_mate/images/solution', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
 
 def index(request):
     subjects = Subject.objects.all()
@@ -23,6 +29,18 @@ def problem(request, subject_id, problem_id):
     context = {"subject_id": subject_id, "problem": problem}
     return render(request, "estudio_mate/answer.html", context)
 
+def solutions(request, subject_id, problem_id):
+    return render(request, "estudio_mate/answer.html", {"answers": ProblemUpload})
+
 @login_required(login_url='/accounts/login/')
 def upload_problem(request, subject_id, problem_id):
-    return render(request, "estudio_mate/submit.html")
+    if request.method == 'POST':
+        form = ProblemUpload(request.POST, request.FILES, subject_id, problem_id)
+        if form.is_valid():
+            form.save()
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/subject_id/problems/problem_id/')
+    else:
+        form = ProblemUpload()
+    return render(request, 'estudio_mate/submit.html', {'form': form})
+
